3c1cba6b04454a9e6c2c83afa5633a876a97bfdd
Stanko <stanko.tadic@gmail.com>
2023-05-16 21:27:06 +0200

    Base config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ui-colors-from-an-image/',
   server: {
     port: 1234,
     host: '0.0.0.0',
